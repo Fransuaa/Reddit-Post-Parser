907521a6fb190e69385dfea62d7bbbbbe677caee
Fransuaa <fran.lopezbenavides@gmail.com>
2026-04-09 23:06:23 -0300

    feat: ej. 1 read subscriptions

--- src/main/scala/FileIO.scala
+++ src/main/scala/FileIO.scala
@@ -1,12 +1,36 @@
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 import scala.io.Source
 
 object FileIO {
-  // Pure function to read subscriptions from a JSON file
-  def readSubscriptions(): List[String] = {
-    List(
-      "https://www.reddit.com/r/scala/.json?count=10",
-      "https://www.reddit.com/r/learnprogramming/.json?count=10"
-    )
+
+  implicit val formats: Formats = DefaultFormats  // lo requiere json4s
+
+  // Impure (IO) function to read subscriptions from a JSON file
+  def readSubscriptions(): Option[List[Main.Subscription]] = {
+    try {
+      val path = "./subscriptions.json"
+      val source = Source.fromFile(path)  // abro stream hacia subscriptions.json (similar a un file descriptor)
+
+      // extraigo contenido del archivo de manera segura
+      val json = try {
+        source.mkString   // extraigo contenido
+      } finally {
+        source.close()  // cierro stream (evito leaks)
+      }
+
+      val content = parse(json)  // parseo json (string) a JValue (estructura de arbol)
+      val subs = content.children.map { item =>
+        val name = (item \ "name").extract[String] // indico donde acceder y que tipo extraer
+        val url = (item \ "url" ).extract[String]
+        (name, url)
+      }
+      Some(subs)
+
+    // si falla (cualquier excepcion) devuelvo None
+    } catch {
+      case _: Exception => None
+    }
   }
 
   // Pure function to download JSON feed from a URL
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,19 +1,29 @@
 object Main {
-  def main(args: Array[String]): Unit = {
+
+  type Subscription = (String, String)  // (subredditName, url)
+
+
+  def main(args: Array[String]): Unit = { 
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    // EJ. 1
+    val subscriptions: Option[List[Subscription]] = FileIO.readSubscriptions()
 
-    val allPosts: List[(String, String)] = subscriptions.map { url =>
-      println(s"Fetching posts from: $url")
-      val posts = FileIO.downloadFeed(url)
-      (url, posts)
+    subscriptions match {
+      case Some(s) => println(s)
+      case None => println("ERROR: No se pudo extraer subscripciones ;(")
     }
-
-    val output = allPosts
-      .map { case (url, posts) => Formatters.formatSubscription(url, posts) }
-      .mkString("\n")
-
-    println(output)
   }
+
+  //   val allPosts: List[Subscription] = subscriptions.map { url =>
+  //     println(s"Fetching posts from: $url")
+  //     val posts = FileIO.downloadFeed(url)
+  //     (url, posts)
+  //   }
+
+  //   val output = allPosts
+  //     .map { case (url, posts) => Formatters.formatSubscription(url, posts) }
+  //     .mkString("\n")
+
+  //   println(output)
 }
